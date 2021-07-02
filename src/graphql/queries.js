f9a0b7fe48ad349348d344125ef0701bd90c324a
Huey Kim <hwuiwon.kim@gmail.com>
2021-07-02 09:03:47 +0900

    Made changes on graphql schema

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,77 +1,20 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const getStockInfo = /* GraphQL */ `
-  query GetStockInfo($stockId: String, $startDate: String) {
-    getStockInfo(stockId: $stockId, startDate: $startDate) {
-      reportList {
-        reportName
-        firm
-        reportIdx
-        date
-        priceGoal
-        analyst
-      }
-      name
-      code
-      companySummary
-      tradePrice
-      changeRate
-      marketCap
-      high52wPrice
-      low52wPrice
-      foreignRatio
-      per
-      pbr
-      roe
-      priceAvg
-      expYield
-      recommend
-      pastData {
-        date
-        start
-        high
-        low
-        end
-        volume
-      }
-      invStatistics {
-        date
-        inKR {
-          individual
-          foreign
-          institutions
-        }
-        inVal {
-          individual
-          foreign
-          institutions
-        }
-      }
-      news {
-        title
-        date
-        source
-        link
-      }
-      newsTitles
-    }
-  }
-`;
 export const getSectorInfo = /* GraphQL */ `
   query GetSectorInfo($sectorName: String, $startDate: String) {
     getSectorInfo(sectorName: $sectorName, startDate: $startDate) {
-      stockList {
-        stockId
-        sSector
-        tradePrice
-        changeRate
-        priceAvg
-        expYield
-        cCount
-        score
-      }
       avgYield
+      stockList {
+        cCount
+        changeRate
+        expYield
+        priceAvg
+        sSector
+        score
+        stockId
+        tradePrice
+      }
       top3List {
         first
         firstYield
@@ -80,6 +23,62 @@ export const getSectorInfo = /* GraphQL */ `
         third
         thirdYield
       }
+    }
+  }
+`;
+export const getStockInfo = /* GraphQL */ `
+  query GetStockInfo($startDate: String, $stockId: String) {
+    getStockInfo(startDate: $startDate, stockId: $stockId) {
+      changeRate
+      code
+      companySummary
+      expYield
+      foreignRatio
+      high52wPrice
+      invStatistics {
+        date
+        inKR {
+          foreign
+          individual
+          institutions
+        }
+        inVal {
+          foreign
+          individual
+          institutions
+        }
+      }
+      low52wPrice
+      marketCap
+      name
+      news {
+        date
+        link
+        title
+      }
+      newsTitles
+      pastData {
+        date
+        end
+        high
+        low
+        start
+        volume
+      }
+      pbr
+      per
+      priceAvg
+      recommend
+      reportList {
+        analyst
+        date
+        firm
+        priceGoal
+        reportIdx
+        reportName
+      }
+      roe
+      tradePrice
     }
   }
 `;
