c5c5420f4f42c93fc4a709b95e49cf4291a11341
Huey Kim <hwuiwon.kim@gmail.com>
2021-07-18 07:58:37 +0900

    Little change

--- src/components/home/StockMarket.js
+++ src/components/home/StockMarket.js
@@ -1,40 +1,51 @@
 import React from 'react';
 
-import "./StockMarket.scss";
+import './StockMarket.scss';
 
-import china from "../../images/china.png";
-import germany from "../../images/germany.png";
-import hongkong from "../../images/hongkong.png";
-import japan from "../../images/japan.png";
-import korea from "../../images/korea.png";
-import uk from "../../images/uk.png";
-import us from "../../images/us.png";
+import china from '../../images/china.png';
+import germany from '../../images/germany.png';
+import hongkong from '../../images/hongkong.png';
+import japan from '../../images/japan.png';
+import korea from '../../images/korea.png';
+import uk from '../../images/uk.png';
+import us from '../../images/us.png';
 
 const StockMarket = ({ data }) => {
-  const marketInfo = index => {
+  const numbWithCommas = (num) => {
+    if (num) {
+      return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    } else {
+      return num;
+    }
+  };
+
+  const marketInfo = (index) => {
     const color = index.changeRate > 0 ? '#e21414' : '#246ded';
     return (
       <>
-        <p style={{color: color}}>{index.tradePrice}</p>
-        <p style={{color: color}}>{index.changePrice}</p>
-        <p style={{color: color}}>{index.changeRate}</p>
+        <p style={{ color: color }}>{numbWithCommas(index.tradePrice)}</p>
+        <p style={{ color: color }}>{index.changePrice}</p>
+        <p style={{ color: color }}>{index.changeRate}%</p>
       </>
-    )
-  }
-  console.log(data.global)
+    );
+  };
+
   return (
     <>
       <div className="index korea">
         <h4>국내증시</h4>
         <div id="market-title">
+          <div></div>
           <div>현재가</div>
           <div>변동</div>
           <div>변동률</div>
         </div>
         <div className="market-table">
-          {data.kr.map(index => (
+          {data.kr.map((index) => (
             <div key={index.symbolCode}>
-              <p><img src={korea} /> {index.name}</p>
+              <p>
+                <img src={korea} alt="kr" /> {index.name}
+              </p>
               {marketInfo(index)}
             </div>
           ))}
@@ -44,41 +55,57 @@ const StockMarket = ({ data }) => {
         <h4>해외증시</h4>
         <div className="market-table">
           <div>
-            <p><img src={china} /> {data.global[0].name}</p>
-            {marketInfo(data.global[0])}
-          </div>
-          <div>
-            <p><img src={japan} /> {data.global[1].name}</p>
-            {marketInfo(data.global[1])}
-          </div>
-          <div>
-            <p><img src={hongkong} /> {data.global[2].name}</p>
-            {marketInfo(data.global[2])}
-          </div>
-          <div>
-            <p><img src={germany} /> {data.global[3].name}</p>
-            {marketInfo(data.global[3])}
-          </div>
-          <div>
-            <p><img src={uk} /> {data.global[4].name}</p>
-            {marketInfo(data.global[4])}
-          </div>
-          <div>
-            <p><img src={us} /> {data.global[5].name}</p>
+            <p>
+              <img src={us} alt="us" /> {data.global[5].name}
+            </p>
             {marketInfo(data.global[5])}
           </div>
           <div>
-            <p><img src={us} /> {data.global[6].name}</p>
+            <p>
+              <img src={us} alt="us" /> {data.global[6].name}
+            </p>
             {marketInfo(data.global[6])}
           </div>
           <div>
-            <p><img src={us} /> {data.global[7].name}</p>
+            <p>
+              <img src={us} alt="us" /> {data.global[7].name}
+            </p>
             {marketInfo(data.global[7])}
+          </div>
+          <div>
+            <p>
+              <img src={china} alt="cn" /> {data.global[0].name}
+            </p>
+            {marketInfo(data.global[0])}
+          </div>
+          <div>
+            <p>
+              <img src={japan} alt="jp" /> {data.global[1].name}
+            </p>
+            {marketInfo(data.global[1])}
+          </div>
+          <div>
+            <p>
+              <img src={hongkong} alt="hk" /> {data.global[2].name}
+            </p>
+            {marketInfo(data.global[2])}
+          </div>
+          <div>
+            <p>
+              <img src={germany} alt="gr" /> {data.global[3].name}
+            </p>
+            {marketInfo(data.global[3])}
+          </div>
+          <div>
+            <p>
+              <img src={uk} alt="uk" /> {data.global[4].name}
+            </p>
+            {marketInfo(data.global[4])}
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default StockMarket;
