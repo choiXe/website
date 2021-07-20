3506cdf70da37cf534c7ccb18a38511482270806
Huey Kim <hwuiwon.kim@gmail.com>
2021-07-20 11:04:44 +0900

    Deleted unnecessary animation

--- src/index.js
+++ src/index.js
@@ -2,23 +2,10 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import Loader from 'react-loader-spinner';
 import './components/setting/i18n';
 
 ReactDOM.render(
-  <Suspense
-    fallback={
-      <div id="loading">
-        <Loader
-          type="MutatingDots"
-          color="#BBD2C5"
-          secondaryColor="#536976"
-          height={100}
-          width={100}
-        />
-      </div>
-    }
-  >
+  <Suspense fallback={<div></div>}>
     <App />
   </Suspense>,
   document.getElementById('root')
