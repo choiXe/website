2d3e6762a8b27965b66d698968d83ffd4867fde7
Sehoan Choi <sehoanchoi0124@gmail.com>
2021-07-29 14:17:53 +0900

    Add dependency to the useEffect that marks a stock 'successfully added' if it's already in favorites

--- src/components/stock/Stock.js
+++ src/components/stock/Stock.js
@@ -37,7 +37,7 @@ const Stock = ({ location }) => {
       .find(stock => stock.stockId === location.state.stockId)
       !== undefined
     )
-  });
+  }, [location.state.stockId]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
